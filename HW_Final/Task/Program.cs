5aac1b9b5dd53f6fac2fed2606eafee12a2cce28
Elvira Yusifzade <elya200909@mail.ru>
2023-05-15 13:58:17 +0300

    Formalized decision

--- HW_Final/Task/Program.cs
+++ HW_Final/Task/Program.cs
@@ -6,3 +6,43 @@
 Console.Write("Enter the number of array elements: ");
 int m = int.Parse(Console.ReadLine()!);
 string [] stringArray = new string [m];
+
+void array(string [] stringArray)
+{
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+     Console.WriteLine($"Enter {i+1} array elements");
+     stringArray[i] = Console.ReadLine();
+  }
+}
+    string [] symbol(string [] stringArray)
+{
+  int n = 0;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+    if(stringArray[i].Length <=3)
+    n++;
+  }
+  string [] rez = new string [n];
+  int j = 0;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+    if(stringArray[i].Length <=3)
+    {
+        rez[j] = stringArray[i];
+        j++;
+    }
+  }
+  return rez;
+}
+void printA(string [] stringArray)
+{
+    Console.Write("[");
+    for (int i = 0;i<stringArray.Length;i++)
+    {
+        Console.Write($"‘{stringArray[i]}’, ");
+    }
+    Console.Write("]");
+}
+array(stringArray);
+printA(symbol(stringArray));
